45317ad0fdf8b983bbea6c04eb40b507b3f4366b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-03-23 20:45:17 +0500

    in working

--- server/router/Controller/Controller.js
+++ server/router/Controller/Controller.js
@@ -9,7 +9,7 @@ export const addProduct = async (req , res) => {
     try {
         const addProductData = await new proModel(req.body)
         addProductData.save()
-        console.log(req.body)
+        res.status(200).json('Product saved')
     } catch (error) {
         res.status(500).json('error wile adding product to database from controller in server : ' , error)
     }
